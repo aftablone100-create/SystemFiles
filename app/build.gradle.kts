7c46879360f7303f08b238dfce5ffa034d3e34a6
Aftab <aftablone100@gmail.com>
2026-08-23 14:11:19 +0530

    Initialize Android application build configuration

---
+++ app/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.systemfiles"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.systemfiles"
+        minSdk = 23
+        targetSdk = 35
+
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
